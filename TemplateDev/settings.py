46a6c5e1f48d0c1bff90fe435bf7c7f95035047f
PriyanshuAgg <pransh20198@gmail.com>
2018-03-20 18:50:37 +0530

    trying sublime git

--- TemplateDev/settings.py
+++ TemplateDev/settings.py
@@ -135,8 +135,3 @@ STATICFILES_DIRS =[
 ]
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static")
-
-
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# STATICFILES_DIRS =( os.path.join(STATIC_ROOT, 'css/'))
