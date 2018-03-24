5bee85659aeead29bfefb9b06bf3892bfc9a7031
Paras Garg <prs.garg@gmail.com>
2018-03-24 22:40:46 +0530

    edit

--- TemplateDev/settings.py
+++ TemplateDev/settings.py
@@ -95,10 +95,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.11/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -108,10 +104,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
 
--- home/views.py
+++ home/views.py
@@ -20,35 +20,6 @@ def post_home(request):
     	"title":"GYMS",
     	}
         return render(request,'base.html',context)
-# def post_list(request):
-#     # if request.user.is_authenticated():
-#     #   context={
-#     #       "title": "my list"
-#     #   }
-#     # else:
-#     #   context={
-#     #       "title": "list"
-#     #   }
-#     queryset_list=Post.objects.all()#.order_by("-timestamp")
-#     paginator = Paginator(queryset_list, 10) # Show 25 contacts per page
-#     page_request_var="page"
-#     page = request.GET.get('page_request_var')
-#     try:
-#         queryset = paginator.page(page)
-#     except PageNotAnInteger:
-#         queryset = paginator.page(1)
-#     except EmptyPage:
-#         queryset = paginator.page(paginator.num_pages)
-
-#     context={
-#             "object_list": queryset,
-#             "title": "my list",
-#             "page_request_var": page_request_var
-#             }
-#     #return render(request,"base.html",context)
-#     return render(request,"post_list.html",context)
- #       return render(request,'base.html',context)
-  #      return render(request,'base.html',context)
 
 def post_detail(request):
     instance = Post.objects.get()
