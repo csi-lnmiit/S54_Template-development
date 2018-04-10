edd781bcf618c006c0bcfa1c37c30c4506cbd7aa
Paras Garg <prs.garg@gmail.com>
2018-04-10 12:58:40 +0530

    edit signup

--- home/views.py
+++ home/views.py
@@ -71,6 +71,21 @@ def photo(request,id=id):
         "obj_photos" : inst_pho,
         } 
     return render(request,'photos.html',context)
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+    
     
 def html_test(request):
     return render(request,'base.html')
